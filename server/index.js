4e3ce509fddecfcefb5202265884241a47e79f1d
fatuousnerd <110926333+Fatuousnerd@users.noreply.github.com>
2025-10-05 22:32:54 +0300

    ++++

--- server/index.js
+++ server/index.js
@@ -1,54 +1,28 @@
-const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const UserModel = require('./models/Users');
-
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const bodyParser = require('body-parser');
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose.connect("mongodb+srv://calvinkmwaura:Fatuous_1764@cluster0.htwpufj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => console.log("Connected to MongoDB successfully!"))
-    .catch(err => console.error("MongoDB connection error:", err));
+//Import Routes
 
-app.get('/', (req, res) => {
-    UserModel.find({})
-        .then(users => res.json(users))
-        .catch(err => res.json(err))
-})
 
-app.get("/getUser/:id", (req, res) => {
-    const id = req.params.id
-    UserModel.findById({ _id: id })
-        .then(users => {
-            res.json(users)
-            console.log(users)
-        })
-        .catch(err => res.json(err))
-});
+mongoose.connect(`${process.env.CONNECTION_STRING_LOCALHOST}/${process.env.DB}`,)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('MongoDB connection error', err));
 
-app.put('/update/:id', (req, res) => {
-    const id = req.params.id;
-    UserModel.findByIdAndUpdate({ _id: id }, { name: req.body.name, email: req.body.email, age: req.body.age })
-        .then(users => res.json(users))
-        .catch(err => res.json(err))
-})
+//Use Routes
 
-app.delete('/delete/:id', (req, res) => {
-    const id = req.params.id
-    UserModel.findByIdAndDelete({ _id: id })
-        .then(res => res.json(res))
-        .catch(err => res.json(err))
-})
 
-// Corrected route to '/create'
-app.post("/create", (req, res) => {
-    UserModel.create(req.body)
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json(err));
-});
-
-app.listen(3001, () => {
-    console.log("Server Is Running!!");
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running. Port: ${process.env.PORT}`);
 });
