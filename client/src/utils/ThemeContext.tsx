25e175785d5d0a0e78ad32e76ee929f33fd97618
fatuousnerd <110926333+Fatuousnerd@users.noreply.github.com>
2025-12-16 09:57:00 +0300

    TS Migration

---
+++ client/src/utils/ThemeContext.tsx
@@ -0,0 +1,52 @@
+import Cookies from "js-cookie";
+import { createContext, useState, useEffect, ReactNode } from "react";
+
+type ThemeType = {
+  theme: string;
+  toggleTheme: () => void;
+};
+
+export const ThemeContext = createContext<ThemeType | undefined>(undefined);
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    // Check system preference for theme
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const themeGet = Cookies.get("theme");
+
+    if (!themeGet) {
+      // If no theme in cookies, set it based on system preference
+      const initialTheme = prefersDark ? "dark" : "light";
+      Cookies.set("theme", initialTheme, { expires: 365 });
+      setTheme(initialTheme);
+    } else {
+      // Set theme from cookie value
+      setTheme(themeGet);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Apply the theme to the body class
+    document.body.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+
+    // Update the cookie and state simultaneously
+    Cookies.set("theme", newTheme, { expires: 365 });
+    setTheme(newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
---
+++ server/index.cts
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+//Import Routes
+
+mongoose
+  .connect(`${process.env.CONNECTION_STRING_LOCALHOST}/${process.env.DB}`)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err: unknown) => console.log("MongoDB connection error", err));
+
+//Use Routes
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running. Port: ${process.env.PORT}`);
+});
